dc178819be11aefbae2cc6e899c5c46c787db801
Haroldo Ramirez da Nobrega <haroldoramirez@gmail.com>
2025-05-16 11:01:44 -0300

    configurado e criado nova evolution

---
+++ conf/evolutions/default/2.sql
@@ -0,0 +1,14 @@
+# --- Sample dataset
+
+# --- !Ups
+
+INSERT INTO jogador (nome) VALUES ('Pumba');
+INSERT INTO jogador (nome) VALUES ('Hoocka');
+INSERT INTO jogador (nome) VALUES ('WK');
+INSERT INTO jogador (nome) VALUES ('Kdu');
+INSERT INTO jogador (nome) VALUES ('Punnys');
+INSERT INTO jogador (nome) VALUES ('DK');
+INSERT INTO jogador (nome) VALUES ('Paulinho');
+INSERT INTO jogador (nome) VALUES ('MASSAS');
+INSERT INTO jogador (nome) VALUES ('Otzi');
+INSERT INTO jogador (nome) VALUES ('Famous');
