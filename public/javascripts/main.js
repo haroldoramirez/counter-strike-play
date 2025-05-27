a3e3b8f49a70a604f36c25892a247bbbb1e252bf
Haroldo Ramirez da Nobrega <haroldoramirez@gmail.com>
2025-05-27 08:34:17 -0300

    Feito melhorias na navbar

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,13 +1,2 @@
 $(document).ready(function () {
-    document.addEventListener("DOMContentLoaded", function () {
-
-        const url = window.location.pathname;
-
-        // Verifique a URL desejada (exemplo: /jogador/sucesso)
-        if (url === "/") {
-            const meuModal = new bootstrap.Modal(document.getElementById('mainModal'));
-            meuModal.show();
-        }
-
-    });
 });
